0f99a446367903132accca1706c9c7fb01b139e4
Dhikshith Reddy <dhikshithreddy12@gmail.com>
2024-11-30 16:58:30 +0530

    fix: android impl

--- android/src/main/kotlin/com/folksable/volume_listener/VolumeListenerActivity.kt
+++ android/src/main/kotlin/com/folksable/volume_listener/VolumeListenerActivity.kt
@@ -5,7 +5,7 @@ import com.folksable.volume_listener.VolumeListenerPlugin.Companion.eventSink
 import io.flutter.embedding.android.FlutterActivity
 
 
-class VolumeListenerActivity(): FlutterActivity() {
+open class VolumeListenerActivity(): FlutterActivity() {
 
     public override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
--- example/android/app/src/main/kotlin/com/folksable/volume_listener_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/folksable/volume_listener_example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.folksable.volume_listener_example
 
+import com.folksable.volume_listener.VolumeListenerActivity
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: VolumeListenerActivity()
