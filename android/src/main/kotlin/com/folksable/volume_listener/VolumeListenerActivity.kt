35dbb75a13293a3bfbe602c88303d116007f75ca
Shreeshant Prajapati <shreeshant@shrigsolutions.com>
2025-09-15 14:23:30 +0545

    system volume trigger disabled

--- android/src/main/kotlin/com/folksable/volume_listener/VolumeListenerActivity.kt
+++ android/src/main/kotlin/com/folksable/volume_listener/VolumeListenerActivity.kt
@@ -2,16 +2,18 @@ package com.folksable.volume_listener
 
 import android.view.KeyEvent
 import com.folksable.volume_listener.VolumeListenerPlugin.Companion.eventSink
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 
-open class VolumeListenerActivity(): FlutterActivity() {
+open class VolumeListenerActivity(): FlutterFragmentActivity() {
 
     public override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
             eventSink?.success("down")
+            return true
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
             eventSink?.success("up")
+            return true
         }
         return super.onKeyDown(keyCode, event)
     }
